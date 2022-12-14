6255098e0fb4e12dabf62df529a268cecdb5d9b6
IleniaTedde <i.tedde@xister.com>
2022-12-14 15:10:40 +0100

    .

--- js/jsLP.js
+++ js/jsLP.js
@@ -1,5 +1,25 @@
-// File esterno chiamato "benvenuti.js"
-function api() { 
-    var links = ['ciao', 'ilenia'];
-   return links;
-}
+document.addEventListener("DOMContentLoaded", () => {
+    var ypos = window.pageYOffset;
+    var height = window.innerHeight;
+    var calc = ypos + height;
+    var iframeArr = document.getElementsByClassName("fullVideo");
+    for(let x = 0; x < iframeArr.length; x++) {
+        if(calc > (iframeArr[x].offsetTop + 200 ) )
+         iframeArr[x].classList.add("visibleFullVideo");
+     }
+
+function Scroll () {
+  var ypos = window.pageYOffset;
+  var height = window.innerHeight;
+  var calc = ypos + height;
+
+  var iframeArr = document.getElementsByClassName("fullVideo");
+    for(let x = 0; x < iframeArr.length; x++) {
+        if(calc > (iframeArr[x].offsetTop + 200 ) )
+         iframeArr[x].classList.add("visibleFullVideo");
+     }
+  }
+
+window.addEventListener("scroll", Scroll);
+
+});
