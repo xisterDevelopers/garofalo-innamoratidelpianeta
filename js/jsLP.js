19e61c918a051f79d452cef3ca4d1af6ceee6d1e
IleniaTedde <i.tedde@xister.com>
2022-12-16 14:00:51 +0100

    animation number

--- js/jsLP.js
+++ js/jsLP.js
@@ -54,6 +54,7 @@ function scrollAncora (id) {
 }
 
 
+
 function collapse () {
   var breakpointMobile = 768;
   var header = document.getElementById("header");
@@ -73,3 +74,78 @@ function collapse () {
     }
   }
 }
+
+
+
+
+
+// ready check helper function (waits for the dom to load, then returns the indivudual elements one at a time - in case there are more than one of them on the page). I generally use this for all of my dom manipulating scripts so I dont have to type DOMContentLoaded and elems.forEach a hundred times
+const ready = (selector, callback) => {
+	window.addEventListener('DOMContentLoaded', function () {
+		const elems = [...document.querySelectorAll(selector)];
+		if (elems.length) {
+			for (let elem of elems) {
+				callback(elem);
+			}
+		}
+	});
+};
+
+ready('.counter', (stat) => {
+	// pattern used to seperate input number from html into an array of numbers and non numbers. EX $65.3M -> ["$65.3M", "$", "65", ".", "3", "M"]
+	const patt = /(\D+)?(\d+)(\D+)?(\d+)?(\D+)?/;
+	const time = 200;
+	let result = [...patt.exec(stat.textContent)];
+	let fresh = true;
+	let ticks;
+	
+	// Remove first full match from result array (we dont need the full match, just the individual match groups).
+	result.shift();
+	// Remove undefined values from result array where they didnt have a match in one of the optional regex groups
+	result = result.filter(res => res != null);
+
+	while (stat.firstChild) {
+		stat.removeChild(stat.firstChild);
+	}
+
+	for (let res of result) {
+		if (isNaN(res)) {
+			stat.insertAdjacentHTML('beforeend', `<span>${res}</span>`);
+		}
+		else {
+			for (let i = 0; i < res.length; i++) {
+				stat.insertAdjacentHTML('beforeend',
+					`<span data-value="${res[i]}">
+						<span>&ndash;</span>
+						${Array(parseInt(res[i]) + 1).join(0).split(0).map((x, j) => `
+							<span>${j}</span>
+						`).join('')}
+					</span>`
+				);
+			}
+		}
+	}
+
+	ticks = [...stat.querySelectorAll('span[data-value]')];
+
+	let activate = () => {
+		let top = stat.getBoundingClientRect().top;
+		let offset = (window.innerHeight * 3 / 4);
+
+		setTimeout(() => {
+			fresh = false;
+		}, time);
+
+		if (top < offset) {
+			setTimeout(() => {
+				for (let tick of ticks) {
+					let dist = parseInt(tick.getAttribute('data-value')) + 1;
+					tick.style.transform = `translateY(-${(dist) * 100}%)`
+				}
+			}, fresh ? time : 0);
+			window.removeEventListener('scroll', activate);
+		}
+	}
+	window.addEventListener('scroll', activate);
+	activate();
+});
